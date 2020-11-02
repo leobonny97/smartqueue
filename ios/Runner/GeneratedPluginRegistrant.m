9832399c50da66de31c37a37035a3e5e9244adba
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-11-02 10:00:00 +0100

    .

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<qrscan/QrscanPlugin.h>)
+#import <qrscan/QrscanPlugin.h>
+#else
+@import qrscan;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [QrscanPlugin registerWithRegistrar:[registry registrarForPlugin:@"QrscanPlugin"]];
 }
 
 @end
---
+++ lib/QR.dart
@@ -0,0 +1,16 @@
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:flutter/material.dart';
+
+// Select Bar-Code or QR-Code photos for analysis
+String photoScanResult = await scanner.scanPhoto();
+
+// Generating QR-Code
+Uint8List result = await scanner.generateBarCode('https://github.com/leyan95/qrcode_scanner');
+
+// Scanning the image of the specified path
+String barcode = await scanner.scanPath(path);
+
+// Parse to code string with uint8list
+File file = await ImagePicker.pickImage(source: ImageSource.camera);
+Uint8List bytes = file.readAsBytesSync();
+String barcode = await scanner.scanBytes(uint8list);
