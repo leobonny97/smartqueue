f950350fe49c51467e41b16b062131aeb940c950
Benny Sommese <benedettosommese@gmail.com>
2020-11-02 01:02:20 +0100

    Aggiunta pulsante e posizionamento elementi

--- lib/main2.dart
+++ lib/main2.dart
@@ -4,7 +4,7 @@ void main() => runApp(MyApp());
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'SmartQueue';
 
   @override
   Widget build(BuildContext context) {
@@ -27,48 +27,63 @@ class MyStatelessWidget extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-
       backgroundColor: Colors.orange,
-      body: Container(
-        child: Card(
-
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-
-          ),
-
-          child: InkWell(
-
-            splashColor: Colors.blue.withAlpha(30),
-            child: Container(
-              alignment: Alignment.topCenter,
-              decoration: BoxDecoration(
-                color: Colors.white,
-
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10)
-                ),
-                boxShadow: [
-                  BoxShadow(
-
-                    color: Colors.black.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 10,
+            right: 10,
+            left: 10,
+            child: Card(
+              shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
               ),
-              width: 300,
-              height: 100,
-              child: Text('Benvenuto'),
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10)
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  width: 300,
+                  height: 100,
+                  child: Text('Benvenuto, \nscannerizza il qr code e partecipa alla fila'),
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: RaisedButton(
+              onPressed: () {
+                print("Button clicked");
+                },
+              color: Colors.blue,
+              elevation: 10,
+              child: Text("Avanti", style: TextStyle(fontSize: 20, color: Colors.white),),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+              shape: StadiumBorder(
+                side: BorderSide(color: Colors.black, width: 1),
+              ),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
