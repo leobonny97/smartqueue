e6a4d72b6fa144674d373ffdad3b20a8aecd3b5d
eeepad86 <47384454+eeepad86@users.noreply.github.com>
2020-12-03 19:06:59 +0100

    update homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smartqueue/benvenuto_Cliente.dart';
 import 'Login_Registrazione.dart';
 
-void main() => runApp(homepage());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(homepage());
+}
 
 /// This is the main application widget.
 class homepage extends StatelessWidget {
