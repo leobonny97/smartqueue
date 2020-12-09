2878cbdce85c3394ecabfe39b92095ee7d577144
eeepad86 <47384454+eeepad86@users.noreply.github.com>
2020-12-09 12:05:42 +0100

    gestioneCoda

--- lib/gestioneCoda.dart
+++ lib/gestioneCoda.dart
@@ -32,6 +32,9 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  String barcode;
+  String id_coda;
+  MyApp({Key key, @required this.barcode, @required this.id_coda}):super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
